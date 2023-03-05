46cdc9eeee5d02a46dd87c31ca2925505725f890
Ivaylo <itrifonov87@gmail.com>
2023-03-05 17:35:06 +0100

    Added Review logic.

---
+++ src/main/java/bg/trifonov/movies/controller/ReviewController.java
@@ -0,0 +1,31 @@
+package bg.trifonov.movies.controller;
+
+import bg.trifonov.movies.domain.Review;
+import bg.trifonov.movies.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/reviews")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String, String> payload) {
+        return new ResponseEntity<>(this.reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/bg/trifonov/movies/service/ReviewService.java
@@ -0,0 +1,37 @@
+package bg.trifonov.movies.service;
+
+import bg.trifonov.movies.domain.Movie;
+import bg.trifonov.movies.domain.Review;
+import bg.trifonov.movies.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final MongoTemplate mongoTemplate;
+
+    @Autowired
+    public ReviewService(ReviewRepository reviewRepository, MongoTemplate mongoTemplate) {
+        this.reviewRepository = reviewRepository;
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = this.reviewRepository.insert(new Review(reviewBody));
+
+        this.mongoTemplate
+                .update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
+
+    }
+
+}
